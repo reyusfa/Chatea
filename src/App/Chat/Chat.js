4e8784ec0cb12f6ebaf7f9bc08f104fd6cf23a62
reyusfa <yusuf.aryanto.1996@gmail.com>
2020-02-22 21:12:11 +0700

    adding giftedchat & test to retrive data from firebase

--- src/App/Chat/Chat.js
+++ src/App/Chat/Chat.js
@@ -1,25 +1,59 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text } from 'react-native';
+// import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
 
-import { GiftedChat } from 'react-native-gifted-chat';
+import { GiftedChat, Bubble } from 'react-native-gifted-chat';
+import { db } from '../../Public/config/firebase';
+
+import { color } from '../../Public/components/Styles';
+
+const objectToArray = object => {
+  return Object.entries(object)
+    .map(([k, v]) => {
+      return {
+        ...v
+      };
+    })
+    .reverse();
+};
 
 const Chat = props => {
   const [messages, setMessages] = useState([]);
 
+  const sendMessages = message => {
+    try {
+      const messageData = {
+        ...message[0]
+      };
+
+      const newMessageKey = db.ref().push().key;
+
+      let updates = {};
+      updates['/messages/' + newMessageKey] = messageData;
+
+      db.ref().update(updates);
+    } catch (error) {
+      // console.log(error);
+    }
+  };
+
+  const getMessages = () => {
+    try {
+      db.ref()
+        .child('messages')
+        .limitToLast(15)
+        .on('value', result => {
+          const data = result.val();
+          setMessages(objectToArray(data));
+          // console.log(objectToArray(data));
+        });
+    } catch (error) {
+      // console.log(error);
+    }
+  };
+
   useEffect(() => {
-    setMessages([
-      {
-        _id: 1,
-        text: 'Hello developer',
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: 'React Native',
-          avatar: 'https://placeimg.com/140/140/any'
-        }
-      }
-    ]);
+    getMessages();
   }, []);
 
   return (
@@ -27,6 +61,38 @@ const Chat = props => {
       messages={messages}
       user={{
         _id: 1
+      }}
+      onSend={message => sendMessages(message)}
+      renderBubble={bubbleProps => {
+        return (
+          <Bubble
+            {...bubbleProps}
+            textStyle={{
+              right: {
+                color: color.Accent1
+              },
+              left: {
+                color: color.Accent2
+              }
+            }}
+            wrapperStyle={{
+              right: {
+                backgroundColor: color.Background
+              },
+              left: {
+                backgroundColor: color.Foreground
+              }
+            }}
+            timeTextStyle={{
+              right: {
+                color: color.Foreground
+              },
+              left: {
+                color: color.Background
+              }
+            }}
+          />
+        );
       }}
     />
   );
