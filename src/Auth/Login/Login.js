64593e9a61ff5b0acf38cd8e09e795a40ce01dea
reyusfa <yusuf.aryanto.1996@gmail.com>
2020-02-23 14:35:03 +0700

    update login & register page

--- src/Auth/Login/Login.js
+++ src/Auth/Login/Login.js
@@ -1,21 +1,107 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { Fragment, useState } from 'react';
+// import { View, Text, StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
 
+import * as yup from 'yup';
+import { useForm } from 'react-hook-form';
+
+import { actionLoginRequest } from '../../Public/redux/action';
+
+// import { color } from '../../Public/components/Styles';
+import {
+  Button,
+  Input,
+  ListItem,
+  ListItemFooter,
+  ContainerScrollView,
+  TextInView
+} from '../../Public/components';
+
+const defaultValues = {
+  email: 'flipchest@gmail.com',
+  password: 'password'
+};
+
+const LoginSchema = yup.object().shape({
+  email: yup.string().required(),
+  password: yup.string().required()
+});
+
 const Login = props => {
+  const { loginRequest, navigation } = props;
+  const [actionLoading, setActionLoading] = useState(false);
+  const { register, handleSubmit, setValue, errors, getValues } = useForm({
+    defaultValues,
+    validationSchema: LoginSchema
+  });
+
+  const handleChange = field => {
+    return text => setValue(field, text, true);
+  };
+
+  const onSubmit = handleSubmit(async () => {
+    setActionLoading(true);
+    const { email, password } = getValues();
+    try {
+      await loginRequest({ email, password });
+      setActionLoading(false);
+    } catch (error) {
+      setActionLoading(false);
+    }
+  });
+
   return (
-    <View>
-      <Text>Login</Text>
-      <Text onPress={() => props.navigation.navigate('Register')}>
-        Register
-      </Text>
-    </View>
+    <ContainerScrollView>
+      <ListItem
+        title={
+          <Fragment>
+            <Input
+              placeholder="Email"
+              ref={register({ name: 'email' })}
+              errorMessage={errors.email ? errors.email.message : ''}
+              onChangeText={handleChange('email')}
+              leftIcon={{ name: 'mail', type: 'material' }}
+            />
+            <Input
+              placeholder="Password"
+              ref={register({ name: 'password' })}
+              errorMessage={errors.password ? errors.password.message : ''}
+              onChangeText={handleChange('password')}
+              leftIcon={{ name: 'lock', type: 'material' }}
+            />
+            <ListItemFooter>
+              <Button
+                title="Login"
+                onPress={onSubmit}
+                loading={actionLoading}
+              />
+            </ListItemFooter>
+            <ListItemFooter>
+              <TextInView
+                title="Create new account."
+                viewProps={{
+                  style: {
+                    padding: 8
+                  }
+                }}
+                textProps={{
+                  onPress: () => navigation.navigate('Register')
+                }}
+                textPrimary
+              />
+            </ListItemFooter>
+          </Fragment>
+        }
+      />
+    </ContainerScrollView>
   );
 };
 
 const mapStateToProps = state => ({});
 
-const mapDispatchToProps = dispatch => ({});
+const mapDispatchToProps = dispatch => ({
+  loginRequest: payload => dispatch(actionLoginRequest(payload))
+});
 
 export default connect(
   mapStateToProps,
--- src/Auth/Register/Register.js
+++ src/Auth/Register/Register.js
@@ -1,18 +1,125 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { Fragment, useState } from 'react';
+// import { View, Text, StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
 
+import * as yup from 'yup';
+import { useForm } from 'react-hook-form';
+
+import { actionRegisterRequest } from '../../Public/redux/action';
+
+// import { color } from '../../Public/components/Styles';
+import {
+  Button,
+  Input,
+  ListItem,
+  ListItemFooter,
+  ContainerScrollView,
+  TextInView
+} from '../../Public/components';
+
+const defaultValues = {
+  email: 'flipchest@gmail.com',
+  password: 'password'
+};
+
+const RegisterSchema = yup.object().shape({
+  email: yup
+    .string()
+    .email()
+    .required(),
+  password: yup.string().required(),
+  reTypePassword: yup
+    .string()
+    .required()
+    .test('password-match', 'Password does not match.', function(value) {
+      return this.parent.password === value;
+    })
+});
+
 const Register = props => {
+  const { registerRequest, navigation } = props;
+  const [actionLoading, setActionLoading] = useState(false);
+  const { register, handleSubmit, setValue, errors, getValues } = useForm({
+    defaultValues,
+    validationSchema: RegisterSchema
+  });
+
+  const handleChange = field => {
+    return text => setValue(field, text, true);
+  };
+
+  const onSubmit = handleSubmit(async () => {
+    setActionLoading(true);
+    const { email, password } = getValues();
+    try {
+      await registerRequest({ email, password });
+      setActionLoading(false);
+    } catch (error) {
+      setActionLoading(false);
+    }
+  });
+
   return (
-    <View>
-      <Text>Register</Text>
-    </View>
+    <ContainerScrollView>
+      <ListItem
+        title={
+          <Fragment>
+            <Input
+              placeholder="Email"
+              ref={register({ name: 'email' })}
+              errorMessage={errors.email ? errors.email.message : ''}
+              onChangeText={handleChange('email')}
+              leftIcon={{ name: 'mail', type: 'material' }}
+            />
+            <Input
+              placeholder="Password"
+              ref={register({ name: 'password' })}
+              errorMessage={errors.password ? errors.password.message : ''}
+              onChangeText={handleChange('password')}
+              leftIcon={{ name: 'lock', type: 'material' }}
+            />
+            <Input
+              placeholder="Re-type Password"
+              ref={register({ name: 'reTypePassword' })}
+              errorMessage={
+                errors.reTypePassword ? errors.reTypePassword.message : ''
+              }
+              onChangeText={handleChange('reTypePassword')}
+              leftIcon={{ name: 'lock', type: 'material' }}
+            />
+            <ListItemFooter>
+              <Button
+                title="Register"
+                onPress={onSubmit}
+                loading={actionLoading}
+              />
+            </ListItemFooter>
+            <ListItemFooter>
+              <TextInView
+                title="Already have an account? Login instead."
+                viewProps={{
+                  style: {
+                    padding: 8
+                  }
+                }}
+                textProps={{
+                  onPress: () => navigation.navigate('Login')
+                }}
+                textPrimary
+              />
+            </ListItemFooter>
+          </Fragment>
+        }
+      />
+    </ContainerScrollView>
   );
 };
 
 const mapStateToProps = state => ({});
 
-const mapDispatchToProps = dispatch => ({});
+const mapDispatchToProps = dispatch => ({
+  registerRequest: payload => dispatch(actionRegisterRequest(payload))
+});
 
 export default connect(
   mapStateToProps,
