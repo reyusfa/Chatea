4c71c9c25462a0d814d0feec5249d3a84c85cfe6
reyusfa <yusuf.aryanto.1996@gmail.com>
2020-02-26 07:38:41 +0700

    add get user function & update redux action payload to promises

--- src/Public/redux/action/users.js
+++ src/Public/redux/action/users.js
@@ -1,6 +1,21 @@
-import { firebaseDatabase, firebaseAuth } from '../../config/firebase';
+import {
+  firebaseDatabase,
+  firebaseAuth,
+  firebaseTimestamp
+} from '../../config/firebase';
 
 const rootRef = firebaseDatabase.ref();
+
+const getUser = ({ userId }) => {
+  return rootRef
+    .child('users')
+    .child(userId)
+    .once('value')
+    .then(result => {
+      const data = result.val() !== null ? result.val() : {};
+      return data;
+    });
+};
 
 const actionGetUser = ({ userId }) => {
   return {
@@ -20,11 +35,15 @@ const actionEditAbout = ({ userId, userAbout }) => {
   return {
     type: 'EDIT_USER_ABOUT',
     payload: new Promise((resolve, reject) => {
-      rootRef
-        .child('users')
-        .child(userId)
-        .child('about')
-        .set(userAbout);
+      // rootRef
+      //   .child('users')
+      //   .child(userId)
+      //   .child('about')
+      //   .set(userAbout);
+      rootRef.update({
+        [`users/${userId}/about`]: userAbout,
+        [`peoples/${userId}/about`]: userAbout
+      });
       resolve(userAbout);
     })
   };
@@ -37,11 +56,15 @@ const actionEditDisplayName = ({ userId, userDisplayName }) => {
       firebaseAuth.currentUser.updateProfile({
         displayName: userDisplayName
       });
-      rootRef
-        .child('users')
-        .child(userId)
-        .child('displayName')
-        .set(userDisplayName);
+      // rootRef
+      //   .child('users')
+      //   .child(userId)
+      //   .child('displayName')
+      //   .set(userDisplayName);
+      rootRef.update({
+        [`users/${userId}/displayName`]: userDisplayName,
+        [`peoples/${userId}/displayName`]: userDisplayName
+      });
       resolve(userDisplayName);
     })
   };
@@ -54,11 +77,15 @@ const actionEditPhotoURL = ({ userId, userPhotoURL }) => {
       firebaseAuth.currentUser.updateProfile({
         photoURL: userPhotoURL
       });
-      rootRef
-        .child('users')
-        .child(userId)
-        .child('photoURL')
-        .set(userPhotoURL);
+      // rootRef
+      //   .child('users')
+      //   .child(userId)
+      //   .child('photoURL')
+      //   .set(userPhotoURL);
+      rootRef.update({
+        [`users/${userId}/photoURL`]: userPhotoURL,
+        [`peoples/${userId}/photoURL`]: userPhotoURL
+      });
       resolve(userPhotoURL);
     })
   };
@@ -71,20 +98,58 @@ const actionEditPhoneNumber = ({ userId, userPhoneNumber }) => {
       firebaseAuth.currentUser.updateProfile({
         phoneNumber: userPhoneNumber
       });
-      rootRef
-        .child('users')
-        .child(userId)
-        .child('phoneNumber')
-        .set(userPhoneNumber);
+      // rootRef
+      //   .child('users')
+      //   .child(userId)
+      //   .child('phoneNumber')
+      //   .set(userPhoneNumber);
+      rootRef.update({
+        [`users/${userId}/phoneNumber`]: userPhoneNumber,
+        [`peoples/${userId}/phoneNumber`]: userPhoneNumber
+      });
       resolve(userPhoneNumber);
     })
   };
 };
 
+const actionAddContact = ({ userId, peopleId }) => {
+  return {
+    type: 'ADD_CONTACT',
+    payload: () => {
+      return new Promise((resolve, reject) => {
+        rootRef
+          .child('users')
+          .child(userId)
+          .child('contacts')
+          .orderByChild('_id')
+          .equalTo(peopleId)
+          .once('value', async contact => {
+            if (!contact.val() && contact.val() === null) {
+              await getUser({ userId: peopleId }).then(user => {
+                rootRef.update({
+                  [`contacts/${userId}/${peopleId}/_id`]: peopleId,
+                  [`contacts/${userId}/${peopleId}/email`]: user.email,
+                  [`contacts/${userId}/${peopleId}/displayName`]: user.displayName,
+                  [`contacts/${userId}/${peopleId}/photoURL`]: user.photoURL,
+                  [`contacts/${userId}/${peopleId}/addedAt`]: firebaseTimestamp
+                });
+                resolve({ peopleId });
+              });
+            } else {
+              reject({ error: 'Contact already added.' });
+            }
+          });
+      });
+    }
+  };
+};
+
 export {
+  getUser,
   actionGetUser,
   actionEditAbout,
   actionEditDisplayName,
   actionEditPhotoURL,
-  actionEditPhoneNumber
+  actionEditPhoneNumber,
+  actionAddContact
 };
