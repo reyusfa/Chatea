43195b8297c98ba6576c81d8ff9de25611d2e0f3
reyusfa <yusuf.aryanto.1996@gmail.com>
2020-02-26 07:28:48 +0700

    implement contact page

--- src/App/Contact/Contact.js
+++ src/App/Contact/Contact.js
@@ -1,18 +1,135 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useCallback, useState } from 'react';
+import { View, FlatList, ActivityIndicator } from 'react-native';
 import { connect } from 'react-redux';
 
+import { ListItem, Avatar } from 'react-native-elements';
+
+import { firebaseDatabase } from '../../Public/config/firebase';
+
+import { objectToArray } from '../../Public/helper';
+
+import { Button } from '../../Public/components';
+import { color, fontFamily } from '../../Public/components/Styles';
+
+import { actionAddChat } from '../../Public/redux/action';
+
 const Contact = props => {
-  return (
-    <View>
-      <Text>Contact</Text>
+  const { auth, navigation, addChat } = props;
+  const [contacts, setContacts] = useState([]);
+  const [renderLoading, setRenderLoading] = useState(false);
+
+  navigation.setOptions({
+    title: 'Contacts'
+  });
+
+  const rootRef = firebaseDatabase.ref();
+  const userId = auth.uid;
+
+  const _getContacts = useCallback(async () => {
+    setRenderLoading(true);
+    try {
+      await rootRef
+        .child('contacts')
+        .child(userId)
+        .on('value', result => {
+          const data = result.val() !== null ? result.val() : {};
+          setContacts(objectToArray(data));
+          setRenderLoading(false);
+        });
+    } catch (error) {
+      // console.log(error);
+      setRenderLoading(false);
+    }
+  }, [rootRef, userId]);
+
+  useEffect(() => {
+    _getContacts();
+  }, [_getContacts]);
+
+  const _addChat = async ({ receiverId }) => {
+    // const receiverId = '8PCQDERsEWb4JQTtkyQvjjRKJlp1';
+    // const receiverId = '1';
+
+    try {
+      await addChat({ senderId: userId, receiverId }).then(result => {
+        // console.log(result);
+        if (result.value.code === 'CHAT_ADDED') {
+          navigation.navigate('Chat', { chatId: result.value.chatId });
+        } else if (result.value.code === 'CHAT_EXISTS') {
+          navigation.navigate('Chat', { chatId: result.value.chatId });
+        }
+      });
+    } catch (error) {
+      // console.log(error);
+    }
+  };
+
+  return renderLoading ? (
+    <View {...{ style: { padding: 16 } }}>
+      <ActivityIndicator size="large" color={color.Foreground} />
+    </View>
+  ) : (
+    <View {...{ style: { backgroundColor: '#ffffff', flex: 1 } }}>
+      <FlatList
+        data={contacts}
+        keyExtractor={item => item._id}
+        renderItem={({ item }) => (
+          <ListItem
+            {...{
+              title: item.displayName || item.email,
+              ...(item.email && item.displayName
+                ? { subtitle: item.email }
+                : {}),
+              titleStyle: { ...fontFamily.Bold },
+              titleProps: { numberOfLines: 1 },
+              containerStyle: {
+                padding: 10,
+                borderColor: color.Accent2,
+                borderBottomWidth: 1
+              },
+              pad: 16
+            }}
+            leftAvatar={
+              <Avatar
+                {...{
+                  title:
+                    (item.displayName && item.displayName[0].toUpperCase()) ||
+                    (item.email && item.email[0].toUpperCase()),
+                  ...(item.photoURL ? { source: { uri: item.photoURL } } : {}),
+                  titleStyle: { color: color.Foreground, ...fontFamily.Bold },
+                  placeholderStyle: {
+                    backgroundColor: color.Background
+                  },
+                  overlayContainerStyle: {
+                    backgroundColor: color.Background,
+                    elevation: 2
+                  },
+                  rounded: true,
+                  size: 55
+                }}
+              />
+            }
+            rightIcon={
+              <Button
+                {...{ buttonStyle: { paddingVertical: 5 } }}
+                title="Chat"
+                onPress={() => _addChat({ receiverId: item._id })}
+              />
+            }
+          />
+        )}
+      />
     </View>
   );
 };
 
-const mapStateToProps = state => ({});
+const mapStateToProps = state => ({
+  auth: state.auth
+});
 
-const mapDispatchToProps = dispatch => ({});
+const mapDispatchToProps = dispatch => ({
+  addChat: payload => dispatch(actionAddChat(payload))
+});
 
 export default connect(
   mapStateToProps,
