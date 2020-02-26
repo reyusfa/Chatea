1b1e181ea5bd4a6c3432d2671e28c27698fb678d
reyusfa <yusuf.aryanto.1996@gmail.com>
2020-02-26 07:37:05 +0700

    update add chat function with detail receiver info

--- src/Public/redux/action/chats.js
+++ src/Public/redux/action/chats.js
@@ -1,6 +1,8 @@
 import { firebaseDatabase, firebaseTimestamp } from '../../config/firebase';
 import { objectToArray } from '../../helper';
 
+import { getUser } from './users';
+
 const rootRef = firebaseDatabase.ref();
 
 const actionGetChats = ({ senderId }) => {
@@ -29,25 +31,44 @@ const actionAddChat = ({ senderId, receiverId }) => {
           .child('chats')
           .orderByChild('receiverId')
           .equalTo(receiverId)
-          .once('value', chatRoom => {
+          .once('value', async chatRoom => {
             if (!chatRoom.val() && chatRoom.val() === null) {
               const newChatKey = rootRef.push().key;
 
+              await getUser({ userId: receiverId }).then(user => {
+                rootRef.update({
+                  [`users/${senderId}/chats/${newChatKey}/_id`]: newChatKey,
+                  [`users/${senderId}/chats/${newChatKey}/receiverId`]: receiverId,
+                  [`users/${senderId}/chats/${newChatKey}/receiverPhotoURL`]: user.photoURL,
+                  [`users/${senderId}/chats/${newChatKey}/receiverDisplayName`]: user.displayName
+                });
+              });
+
+              await getUser({ userId: senderId }).then(user => {
+                rootRef.update({
+                  [`users/${receiverId}/chats/${newChatKey}/_id`]: newChatKey,
+                  [`users/${receiverId}/chats/${newChatKey}/receiverId`]: senderId,
+                  [`users/${receiverId}/chats/${newChatKey}/receiverPhotoURL`]: user.photoURL,
+                  [`users/${receiverId}/chats/${newChatKey}/receiverDisplayName`]: user.displayName
+                });
+              });
+
               rootRef.update({
-                [`users/${senderId}/chats/${newChatKey}/_id`]: newChatKey,
-                [`users/${senderId}/chats/${newChatKey}/receiverId`]: receiverId,
                 [`users/${senderId}/chats/${newChatKey}/updatedAt`]: firebaseTimestamp,
 
-                [`users/${receiverId}/chats/${newChatKey}/_id`]: newChatKey,
-                [`users/${receiverId}/chats/${newChatKey}/receiverId`]: senderId,
                 [`users/${receiverId}/chats/${newChatKey}/updatedAt`]: firebaseTimestamp,
 
                 [`chats/${newChatKey}/_id`]: newChatKey,
                 [`chats/${newChatKey}/updatedAt`]: firebaseTimestamp
               });
-              resolve({ chatId: newChatKey });
+              resolve({ code: 'CHAT_ADDED', chatId: newChatKey });
             } else {
-              reject({ error: 'Chat ID already exits.' });
+              // console.log(Object.keys(chatRoom.val())[0]);
+              resolve({
+                code: 'CHAT_EXISTS',
+                chatId:
+                  Object.keys(chatRoom.val()) && Object.keys(chatRoom.val())[0]
+              });
             }
           });
       });
